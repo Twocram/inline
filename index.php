1eb5c7f36a1a500a2a9229b97aff3e6920b2f3e6
Twocram <2002maslovartem@gmail.com>
2022-08-23 14:02:29 +0500

    added search and output of posts with comments

--- index.php
+++ index.php
@@ -9,7 +9,39 @@
 </head>
 
 <body>
-<?php require "db_connect.php"; ?>
+    <form action="load_data.php" method="post">
+        <button>Добавить данные</button>
+    </form>
+    <hr>
+
+    <form method="get">
+        <input type="text" name="search" id="search">
+        <input type="submit" name="find" value="Найти">
+    </form>
+
+    <?php
+    require "db_connect.php";
+    $find = $_GET['find'];
+    $search = $_GET['search'];
+
+    if (isset($find) && strlen($search) > 2) {
+        $sql = "SELECT post.title, comment.body FROM `post` INNER JOIN `comment` ON `post`.`id` = `comment`.`post_id` WHERE (`comment`.`body` LIKE '%$search%')";
+        $result = $connection->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<div>"
+                    . "title: " . $row["title"] .
+                    "<div>" . "comment body:" . $row["body"] . "</div>" .
+                    "</div>";
+            }
+        } else {
+            echo "По вашему запросу ничего не найдено";
+        }
+    } else {
+        echo "Строка должна содержать минимум 3 символа";
+    }
+
+    ?>
 
 </body>
 
