97291548e91134cfb4e22c2bdf155b0ad3c616fe
Twocram <2002maslovartem@gmail.com>
2022-08-23 15:10:32 +0500

    added styles and changed tabs

--- find_posts.php
+++ find_posts.php
@@ -1,21 +1,21 @@
 <?php
-    require "db_connect.php";
-    $find = $_GET['find'];
-    $search = $_GET['search'];
+require "db_connect.php";
+$find = $_GET['find'];
+$search = $_GET['search'];
 
-    if (isset($find) && strlen($search) > 2) {
-        $sql = "SELECT post.title, comment.body FROM `post` INNER JOIN `comment` ON `post`.`id` = `comment`.`post_id` WHERE (`comment`.`body` LIKE '%$search%')";
-        $result = $connection->query($sql);
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo "<div>"
-                    . "title: " . $row["title"] .
-                    "<div>" . "comment body:" . $row["body"] . "</div>" .
-                    "</div>";
-            }
-        } else {
-            echo "По вашему запросу ничего не найдено";
+if (isset($find) && strlen($search) > 2) {
+    $sql = "SELECT post.title, comment.body FROM `post` INNER JOIN `comment` ON `post`.`id` = `comment`.`post_id` WHERE (`comment`.`body` LIKE '%$search%')";
+    $result = $connection->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<div class='post'>"
+                . "<div class='post-title'>" . $row["title"] . "</div>" .
+                "<div class='post-comment'>" . $row["body"] . "</div>" .
+                "</div>";
         }
     } else {
-        echo "Строка должна содержать минимум 3 символа";
+        echo "По вашему запросу ничего не найдено";
     }
+} else {
+    echo "Строка должна содержать минимум 3 символа";
+}
