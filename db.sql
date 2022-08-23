cdc796b8a0d21d3aa3289beb749ad899d4afa4dc
Twocram <2002maslovartem@gmail.com>
2022-08-23 11:02:31 +0500

    first commit

---
+++ db.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA `inline` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `inline`.`post` (
+                                 `id` INT NOT NULL,
+                                 `body` VARCHAR(255) NOT NULL,
+                                 `title` VARCHAR(255) NOT NULL,
+                                 PRIMARY KEY (`id`),
+                                 UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+CREATE TABLE `inline`.`comment` (
+                                    `id` INT NOT NULL,
+                                    `post_id` INT NOT NULL,
+                                    `name` VARCHAR(255) NOT NULL,
+                                    `email` VARCHAR(255) NOT NULL,
+                                    `body` VARCHAR(255) NOT NULL,
+                                    PRIMARY KEY (`id`),
+                                    INDEX(`post_id`),
+                                    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+
